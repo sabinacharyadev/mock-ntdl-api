5eb41f719989cd38c2aeb67d7d30fa6af387f2d7
sabinbabu <sabinacharya444@gmail.com>
2025-02-15 21:14:42 +1100

    get route added for index and show

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 const TASK_LIST = [
   {
     id: "1",
@@ -10,16 +12,48 @@ const TASK_LIST = [
     time: "1 hrs",
   },
   {
-    id: "1",
+    id: "2",
     task: "TASK 1",
     time: "1 hrs",
   },
   {
-    id: "1",
+    id: "3",
     task: "TASK 1",
     time: "1 hrs",
   },
 ];
+
+// //////////////////////
+// GET | INDEX
+app.get("/api/v1/taskList", (req, res) => {
+  res.json({
+    message: "Data fetched successfully",
+    code: 200,
+    status: "Success",
+    data: TASK_LIST,
+  });
+});
+
+// //////////////////////
+// GET | SHOW
+app.get("/api/v1/taskList/:id", (req, res) => {
+  const { id } = req.params;
+
+  const task = TASK_LIST.find((item) => item.id === id);
+  if (task) {
+    const task = TASK_LIST.find((item) => item.id === id);
+    res.json({
+      message: "Task found",
+      data: task,
+    });
+  }
+});
+
+// ////////////////////////////
+// POST
+app.post("/api/v1/taskList", (req, res) => {
+  const { id, task, time } = req.body;
+});
 
 // START THE SERVER
 app.listen(PORT, (error) => {
