8a8baa8ddc90348451b6d09508f79d9e7b4ac66a
sabinbabu <sabinacharya444@gmail.com>
2025-02-15 20:49:46 +1100

    mock data added

--- server.js
+++ server.js
@@ -3,6 +3,24 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+const TASK_LIST = [
+  {
+    id: "1",
+    task: "TASK 1",
+    time: "1 hrs",
+  },
+  {
+    id: "1",
+    task: "TASK 1",
+    time: "1 hrs",
+  },
+  {
+    id: "1",
+    task: "TASK 1",
+    time: "1 hrs",
+  },
+];
+
 // START THE SERVER
 app.listen(PORT, (error) => {
   error
